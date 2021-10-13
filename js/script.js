a3092e3189cdf7151addae5dea8c22a0c6f6cc25
João Junior <joao_junior_14@hotmail.com>
2021-10-13 18:01:06 -0300

    atualização visual do header, efeito de digitalização

--- js/script.js
+++ js/script.js
@@ -10,3 +10,21 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+
+  
+var string = "Olá, sou João de Oliveira Júnior"; /* type your text here */
+var array = string.split("");
+var timer;
+
+function frameLooper () {
+	if (array.length > 0) {
+		document.getElementById("hello").innerHTML += array.shift();
+	} else {
+		clearTimeout(timer);
+			}
+	loopTimer = setTimeout('frameLooper()',100); /* change 70 for speed */
+
+}
+frameLooper();
